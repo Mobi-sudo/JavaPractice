e5989fb56628f172cb373a59d1ceb1d14253ca6a
Mobi-sudo <banjobaldon@yahoo.com>
2025-03-18 12:08:41 +0800

    commit Changes

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -3,30 +3,34 @@ import java.util.Scanner;
 public class CurrencyConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int peso = 0;
+        while(true){
+            System.out.print("Enter Peso amount: ");
+            if(scanner.hasNextInt()){
+                peso = scanner.nextInt();
+                break;
+            }else{
+                System.out.println("Invalid input. Only Number value is allowed");
+                
+            }                  
+        }
+
         System.out.print("Pick USDT Or Yen For Conversion: ");
         String Moneytoconvert = scanner.next();
-    
         switch(Moneytoconvert.toUpperCase()){
             case "USDT":
-                while (true) {
-                    System.out.println("Enter peso Amount to convert for USDT: ");
-                    int pesousdt = scanner.nextInt();
-                    if(pesousdt >= 50){
-                        int usdt = pesousdt / 50;
-                        System.out.println("Total USDT converted: " + usdt);
-                        break;
-                    }else{
-                        System.out.println("Please input atleast 50 or above.");
-                    }
-                    break;
+                if(peso >= 50){
+                    int usdt = peso / 50;
+                    System.out.println("Total USDT converted: " + usdt);
+                }else{
+                    System.out.println("Please input atleast 50 or above.");
                 }
+                break;
             case "Yen":
                 System.out.println("Enter Peso Amount to convert for Yen: ");
-                int pesoyen = scanner.nextInt();
-                if(pesoyen>= 1){
-                    double yen = pesoyen/0.39;
+                if(peso>= 1){
+                    double yen = peso/0.39;
                     System.out.println("Total Yen Converted: " + yen);
-                    break;
                 }else{
                     System.out.println("Please input at least 1 or above");
                 }
